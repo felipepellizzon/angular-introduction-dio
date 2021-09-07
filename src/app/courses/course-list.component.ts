3ffc538a280694ead86d4d36360bd325df733cb7
felipepellizzon <fapellizzon@gmail.com>
2021-09-07 03:17:45 -0300

    Introduction to Angular 8 - part 2 - DIO

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,38 +1,34 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course"
+import { CourseService } from "./course.service";
 
 @Component({
-    selector: 'app-course-list',
+    
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit{
 
-    courses: Course[] = [];
+    _courses: Course[] = [];
+
+    _filterBy!: string;
+    _filteredCourses: Course[] = [];
+
+    constructor(private courseService: CourseService){}
 
 
     ngOnInit(): void{
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageurl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-8796',
-                duration: 40,
-                rating: 4,
-                releaseDate: 'December, 2, 2019'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageurl: '/assets/images/http.png',
-                price: 45.99,
-                code: 'LKL-1995',
-                duration: 80,
-                rating: 4.5,
-                releaseDate: 'November, 22, 2019'
-            }
-        ]
+        this._courses = this.courseService.retrieveAll();
+        this._filteredCourses=this._courses;
+    }
+
+    set filter(value: string){
+        this._filterBy = value;
+
+        this._filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1)
+    }
+
+    get filter(){
+        return this._filterBy;
     }
 
 }
